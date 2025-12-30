8011bff9f4871bc01375334401f5d8d23f6ffd62
AliAHmadAyoob <aliahamdayoob@gmail.com>
2025-12-30 09:15:24 +0500

    Updated app,requirements

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
 import streamlit as st
-import gradio as gr
+from ultralytics import YOLO
+from IPython.display import Image
+
+@st.cache_resource
+def load_model():
+    return YOLO('licese-plate.pt')
+model = load_model()
 
 st.set_page_config(page_title="License Plate Recognition", page_icon="🚗")
 
 st.title("🚘 License Plate Recognition")
-st.write("Upload an image of a vehicle to recognize its license plate.")
-st.components.v1.iframe("http://127.0.0.1:7860", height=600,scrolling=True)
+st.write("Upload an image or video of a vehicle to recognize license plate.")
+uploaded = st.file_upload('Choose an image...',type=['jpg','jpeg','png'])
+
+if uploaded is not None:
+    if uploaded.type.startswith('image'):
+        img = Image.open(uploaded).convet("RGB")
+        st.image(img,caption='uploaded image',use_column_width=True)
+
+        pred = model.predict(img)
+        pred[0].plot()[ :,:,::-1]
